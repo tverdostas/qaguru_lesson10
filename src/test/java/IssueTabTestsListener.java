2df9aa8a0f33209dc846d3546a6ff9cf6fed8e5d
Anastasiya Elkafrawi <atverdova78@gmail.com>
2025-08-01 00:10:45 +0400

    homework in progress

--- src/test/java/IssueTabTestsListener.java
+++ src/test/java/IssueTabTestsListener.java
@@ -1,4 +1,6 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.byText;
@@ -8,7 +10,10 @@ import static com.codeborne.selenide.Selenide.open;
 public class IssueTabTestsListener {
     @Test
             void IssueTabListenerTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
         open("https://github.com");
+
         $("span.flex-1[data-target='qbsearch-input.inputButtonText']").click();
         $("input#query-builder-test").setValue("selenide").pressEnter();
         $("data-testid='results-list'").$(byText("selenide/selenide")).click();
